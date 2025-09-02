876d718673e07944337a900abac1e880d45133b1
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-02 10:15:49 +0200

    Creazione calcolo del livello e del bonus competenza

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -52,3 +52,73 @@ document.getElementById('arma_5').addEventListener('click', () => {
   data.armi.forEach(arma => console.log(`${arma.nome} - ${arma.descrizione}`));
 });
 
+//Inserimento automatico del livello
+let inputXp = document.getElementById("xp");
+let inputBonusLivello = document.getElementById("bonus_ct_li");
+let playerLevel = document.getElementById("livello");
+
+document.getElementById('xp').addEventListener('input', () => {
+  let xp = parseInt(inputXp.value) || 0;
+
+  if ((xp >= 0) && (xp < 300)){
+        playerLevel.textContent = "1";
+        inputBonusLivello.textContent = "+2"
+  } else if ((xp >= 300) && (xp < 900)){
+        playerLevel.textContent = "2";
+        inputBonusLivello.textContent = "+2"
+  } else if ((xp >= 900) && (xp < 2700)){
+        playerLevel.textContent = "3";
+        inputBonusLivello.textContent = "+2"
+  }  else if ((xp >= 2700) && (xp < 6500)){
+        playerLevel.textContent = "4";
+        inputBonusLivello.textContent = "+2"
+  } else if ((xp >= 6500) && (xp < 14000)){
+        playerLevel.textContent = "5";
+        inputBonusLivello.textContent = "+3"
+  } else if ((xp >= 14000) && (xp < 23000)){
+        playerLevel.textContent = "6";
+        inputBonusLivello.textContent = "+3"
+  } else if ((xp >= 23000) && (xp < 34000)){
+        playerLevel.textContent = "7";
+        inputBonusLivello.textContent = "+3"
+  } else if ((xp >= 34000) && (xp < 48000)){
+        playerLevel.textContent = "8";
+        inputBonusLivello.textContent = "+3"
+  } else if ((xp >= 48000) && (xp < 64000)){
+        playerLevel.textContent = "9";
+        inputBonusLivello.textContent = "+4"
+  } else if ((xp >= 64000) && (xp < 85000)){
+        playerLevel.textContent = "10";
+        inputBonusLivello.textContent = "+4"
+  } else if ((xp >= 85000) && (xp < 100000)){
+        playerLevel.textContent = "11";
+        inputBonusLivello.textContent = "+4"
+  } else if ((xp >= 100000) && (xp < 120000)){
+        playerLevel.textContent = "12";
+        inputBonusLivello.textContent = "+4"
+  } else if ((xp >= 120000) && (xp < 140000)){
+        playerLevel.textContent = "13";
+        inputBonusLivello.textContent = "+5"
+  } else if ((xp >= 140000) && (xp < 165000)){
+        playerLevel.textContent = "14";
+        inputBonusLivello.textContent = "+5"
+  } else if ((xp >= 165000) && (xp < 195000)){
+        playerLevel.textContent = "15";
+        inputBonusLivello.textContent = "+5"
+  } else if ((xp >= 195000) && (xp < 225000)){
+        playerLevel.textContent = "16";
+        inputBonusLivello.textContent = "+5"
+  } else if ((xp >= 225000) && (xp < 265000)){
+        playerLevel.textContent = "17";
+        inputBonusLivello.textContent = "+6"
+  } else if ((xp >= 265000) && (xp < 305000)){
+        playerLevel.textContent = "18";
+        inputBonusLivello.textContent = "+6"
+  } else if ((xp >= 305000) && (xp < 355000)){
+        playerLevel.textContent = "19";
+        inputBonusLivello.textContent = "+6"
+  } else if ((xp >= 3400)) {
+        playerLevel.textContent = "20";
+        inputBonusLivello.textContent = "+6"
+  } 
+})
